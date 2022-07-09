936a6cceea4efb178a6584176ed123baa9d15ead
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-07-09 08:18:47 +0200

    page404

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import HeroNotFound from "../../components/HeroNotFound/HeroNotFound";
+
+const NotFound = () => {
+  return (
+    <main>
+      <HeroNotFound />
+    </main>
+  );
+};
+
+export default NotFound;
