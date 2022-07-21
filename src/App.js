fbd50603f30f730a7122bcf59bda85ae1c45f6ad
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-07-21 10:56:48 +0200

    transactions between 1 and 3 fixed

--- src/App.js
+++ src/App.js
@@ -21,7 +21,11 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/transactions/:accountId" element={<Transactions />} />
+          <Route
+            exact
+            path="/transactions/:accountId"
+            element={<Transactions />}
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
--- src/pages/transactions/transactions.jsx
+++ src/pages/transactions/transactions.jsx
@@ -9,20 +9,23 @@ import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { userBackgroundBlack } from "../../features/auth/authSlice";
+import NotFound from "../NotFound/NotFound";
 
 const Transactions = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   let { accountId } = useParams();
 
-  const Single_Account_Mock = infoProfile.find(
+  const AccountMockLenght = infoProfile.length;
+  const SingleAccountMock = infoProfile.find(
     (account) => account.id === parseInt(accountId)
   );
 
   const { user, isBackground } = useSelector((state) => state.auth);
 
   const backButton = () => {
-    navigate(-1);
+    navigate("/profile");
     if (isBackground) {
       dispatch(userBackgroundBlack());
     }
@@ -37,45 +40,55 @@ const Transactions = () => {
   if (user) {
     return (
       <main>
-        <button className="back-button" onClick={backButton}>
-          <span className="back-icon">
-            <FontAwesomeIcon icon={faArrowLeftLong} />
-          </span>
+        {accountId > AccountMockLenght || accountId < 1 ? (
+          <div></div>
+        ) : (
+          <button className="back-button" onClick={backButton}>
+            <span className="back-icon">
+              <FontAwesomeIcon icon={faArrowLeftLong} />
+            </span>
 
-          <span className="back-text">Back</span>
-        </button>
+            <span className="back-text">Back</span>
+          </button>
+        )}
 
-        <TransactionsAccount
-          key={Single_Account_Mock.id}
-          title={Single_Account_Mock.title}
-          amount={Single_Account_Mock.amount}
-          amountDescription={Single_Account_Mock.amountDescription}
-        />
-        <section className="main bg-light">
-          <h2 className="sr-only">transactions list</h2>
-          <div className="transactions">
-            <div className="transactions-wrapper">
-              <div className="transactions-list-title">
-                <p className="transactions-list-text"></p>
-                <p className="transactions-list-text">DATE</p>
-                <p className="transactions-list-text">DESCRIPTION</p>
-                <p className="transactions-list-text">AMOUNT</p>
-                <p className="transactions-list-text">BALANCE</p>
+        {accountId > AccountMockLenght || accountId < 1 ? (
+          <NotFound />
+        ) : (
+          <>
+            <TransactionsAccount
+              key={SingleAccountMock.id}
+              title={SingleAccountMock.title}
+              amount={SingleAccountMock.amount}
+              amountDescription={SingleAccountMock.amountDescription}
+            />
+            <section className="main bg-light">
+              <h2 className="sr-only">transactions list</h2>
+              <div className="transactions">
+                <div className="transactions-wrapper">
+                  <div className="transactions-list-title">
+                    <p className="transactions-list-text"></p>
+                    <p className="transactions-list-text">DATE</p>
+                    <p className="transactions-list-text">DESCRIPTION</p>
+                    <p className="transactions-list-text">AMOUNT</p>
+                    <p className="transactions-list-text">BALANCE</p>
+                  </div>
+                  <div className="accordeons-list">
+                    {infoTransactions.map((props, index) => (
+                      <Accordeons
+                        date={props.date}
+                        description={props.description}
+                        amount={props.amount}
+                        balance={props.balance}
+                        key={index}
+                      />
+                    ))}
+                  </div>
+                </div>
               </div>
-              <div className="accordeons-list">
-                {infoTransactions.map((props, index) => (
-                  <Accordeons
-                    date={props.date}
-                    description={props.description}
-                    amount={props.amount}
-                    balance={props.balance}
-                    key={index}
-                  />
-                ))}
-              </div>
-            </div>
-          </div>
-        </section>
+            </section>
+          </>
+        )}
       </main>
     );
   }
